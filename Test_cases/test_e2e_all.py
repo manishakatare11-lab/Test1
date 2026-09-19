c31744c0db8daa85585d9d29ec5dd58bc9f04bd3
Manisha Katare <manisha.katare@shl.com>
2026-09-19 21:46:53 +0530

    manisha_19_sep

--- Test_cases/test_e2e_all.py
+++ Test_cases/test_e2e_all.py
@@ -8,10 +8,12 @@ class TestE2EFlow:
     def test_schedule_interview(self, login_setup):
         schedule_test = ScheduleFlow()
         schedule_test.test_schedule_interview(login_setup)
+        print("schedule test ")
 
     def test_join_interview(self):
         join_test = JoinFlow()
         join_test.test_join_interview()
+        print("join test ")
 
 
 
